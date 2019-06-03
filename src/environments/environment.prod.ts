9b79bfcb046e6d44a4dd3ba637005a2974133856
miguelyoobic95 <mmarin@yoobic.com>
2019-06-03 21:30:40 +0100

    chore(app): add firebase to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD1C5afpEolDHdut04FL2SnJQaWXTVTApo",
+    authDomain: "survey-checker.firebaseapp.com",
+    databaseURL: "https://survey-checker.firebaseio.com",
+    projectId: "survey-checker",
+    storageBucket: "",
+    messagingSenderId: "1087328509812",
+    appId: "1:1087328509812:web:71ece80d79cbeaa5"
+  }
 };
